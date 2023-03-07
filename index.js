b6fe8b042c12accb9a4b9299960ee1571e113297
mishael joseph <mishaeljoe55@gmail.com>
2023-03-07 16:14:15 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+let weather = {
+    "apikey": "3a3d4cd1b98f817427cab8e6fc56692a",
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=metric&appid=" 
+            + this.apikey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        //console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText = `weather in ${name}`;
+        document.querySelector(".temp").innerText = `${temp}°C`;
+        document.querySelector(".description").innerText = `${description}`;
+        document.querySelector(".icon").innerText = `${icon}`;
+        document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`;
+        document.querySelector(".wind").innerText = `Wind speed: ${speed}km/h`;
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value)
+    }
+};
+
+document.querySelector(".search button").addEventListener("click", function() {
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+    if(event.key == "Enter") {
+        weather.search();
+    }
+})
+
+// const successCallback = (position) => {
+//     console.log(position);
+//   };
+  
+//   const errorCallback = (error) => {
+//     console.log(error);
+//   };
+  
+//   navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+//weather.fetchWeather("navigator.geolocation.getCurrentPosition(successCallback, errorCallback)")
